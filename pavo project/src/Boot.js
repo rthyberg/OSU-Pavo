9dc714ca71597e08f24158e5a75f3660cbb526b7
Ving Trung <vtrung>
2017-01-24 23:13:34 -0800

    Merge branch 'master' into thybergr-map
    # Conflicts:
    #	pavo project/.DS_Store
    #	pavo project/index.html
    #	pavo project/src/Boot.js
    #	pavo project/src/Preloader.js

--- pavo project/src/Boot.js
+++ pavo project/src/Boot.js
@@ -5,11 +5,17 @@ TowerDefense.Boot = function(game) {};
 
 TowerDefense.Boot.prototype = {
     preload: function() {
+<<<<<<< HEAD
         this.load.image('cat', 'img/loading/eve.jpg');
+=======
+        //this.load.image('preloaderBar', 'images/loader_bar.png');
+        //this.load.image('titleimage', 'images/TitleImage.png');
+>>>>>>> master
     },
 
     create: function() {
         this.input.maxPointers = 1;
+<<<<<<< HEAD
         this.stage.disableVisibilityChange = false;
 		    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		    this.scale.minWidth = 800;
@@ -22,6 +28,24 @@ TowerDefense.Boot.prototype = {
 		    this.input.addPointer();
 		    this.stage.backgroundColor = '#171642';
 
+=======
+		this.stage.disableVisibilityChange = false;
+        
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.minWidth = 800;
+		this.scale.minHeight = 600;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.stage.forcePortrait = true;
+		//this.scale.setScreenSize(true);
+
+		this.input.addPointer();
+		//this.stage.backgroundColor = '#171642';
+        
+        this.stage.backgroundColor = "#4488AA";
+        
+>>>>>>> master
         this.state.start('Preloader');
+        
     }
 };
