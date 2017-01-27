78d66714b20525cdd967d3a252f994417cc21c0d
aadbfs <aadbfs@gmail.com>
2017-01-26 16:43:06 -0800

    Alex fixes

--- pavo project/src/Boot.js
+++ pavo project/src/Boot.js
@@ -5,24 +5,28 @@ TowerDefense.Boot = function(game) {};
 
 TowerDefense.Boot.prototype = {
     preload: function() {
-        this.load.image('preloaderBar', 'images/loader_bar.png');
-        this.load.image('titleimage', 'images/TitleImage.png');
+        this.load.image('cat', 'img/loading/eve.jpg');
+        //this.load.image('preloaderBar', 'images/loader_bar.png');
+        //this.load.image('titleimage', 'images/TitleImage.png');
     },
-    
+
     create: function() {
         this.input.maxPointers = 1;
 		this.stage.disableVisibilityChange = false;
+        
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.minWidth = 800;
 		this.scale.minHeight = 600;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 		this.stage.forcePortrait = true;
-		this.scale.setScreenSize(true);
+		//this.scale.setScreenSize(true);
 
 		this.input.addPointer();
-		this.stage.backgroundColor = '#171642';
+		
+        //this.stage.backgroundColor = "#4488AA";
         
         this.state.start('Preloader');
+        
     }
-}
+};
