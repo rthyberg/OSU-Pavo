b2dbe7a8901c92d86c8ad7c92af570f3e8669a8e
Robert Thyberg <robertthyberg@gmail.com>
2017-01-28 18:57:19 -0800

    changed images->img for loading sprites

--- pavo project/src/Preloader.js
+++ pavo project/src/Preloader.js
@@ -12,7 +12,7 @@ TowerDefense.Preloader.prototype = {
         /**********MAP*******************/
         this.game.load.image('tiles', 'img/ground.png');
         /**********SPRITES*******************/
-        this.game.load.spritesheet('mummy', 'images/placeholder-enemy.png', 64, 64, 180);
+        this.game.load.spritesheet('mummy', 'img/placeholder-enemy.png', 64, 64, 180);
         /**********AUDIO*******************/
 
         this.state.start('Game'); // changes state to Game.js
