2430a01bf24adf9b9f7381f4e9f206216adcd065
Robert Thyberg <robertthyberg@gmail.com>
2017-02-07 15:35:25 -0800

    Added Tower button class to Tower.js and added example usage to Game and LevelOne

--- pavo project/src/LevelOne.js
+++ pavo project/src/LevelOne.js
@@ -27,25 +27,33 @@ TowerDefense.LevelOne.prototype = {
         var layer = map.createLayer('world1');
         layer.resizeWorld();
         layer.wrap = true;
-        
+
+        // Towers
+        this.towerList = Tower.createGroup(this);
+
+        this.uibutton = new createTowerButton(this, 300, 500, 'tower', 'tower', this.towerList);
+        this.uibutton.create();
+
+
+
         //this.fire = this.add.group();
         this.enemies = this.add.group();
-        
+
         this.buildEmitter();
         this.loop = game.time.events.loop(500, this.loadEnemies, this);
-//        
+//
 	},
 
-	
-    
+
+
     render: function(){
         game.debug.text("Group size: " + this.enemies.total, 32, 32);
         //game.debug.text("Destroyed: " + rip, 32, 64);
     },
-    
+
     loadEnemies: function(){
-        var randomY = game.rnd.integerInRange(200, 400);     
-        var zombie = this.enemies.create(0, randomY, 'zombie'); 
+        var randomY = game.rnd.integerInRange(200, 400);
+        var zombie = this.enemies.create(0, randomY, 'zombie');
         zombie.life = 10;
         zombie.animations.add('walk');
         zombie.play('walk', 10, true);
@@ -53,14 +61,14 @@ TowerDefense.LevelOne.prototype = {
         zombie.enableBody = true;
         this.totalspawn++;
     },
-    
+
     checkEnemy: function(enemy){
         if(this.totalspawn > 25){
             game.time.events.remove(this.loop);
         }
         try {
             //console.log(game.width);
-            
+
             if (enemy.x > game.width)
             {
                 //rip++;
@@ -76,19 +84,19 @@ TowerDefense.LevelOne.prototype = {
         }
 
     },
-    
+
     buildEmitter:function() {
         this.fire = this.add.group();
         this.input.onDown.add(this.fireBurst, this);
     },
-    
+
     fireBurst: function(pointer) {
         var f = this.fire.create(pointer.x, pointer.y, 'explosion');
         f.time = 2;
         f.animations.add('burst');
         this.physics.enable(f, Phaser.Physics.ARCADE);
     },
-    
+
     checkFire: function(f){
         console.log(f.time);
         try {
@@ -103,7 +111,7 @@ TowerDefense.LevelOne.prototype = {
             console.log(f);
         }
     },
-    
+
     fireCollision: function(enemy, fire){
         console.log(enemy);
         console.log(fire);
@@ -111,9 +119,10 @@ TowerDefense.LevelOne.prototype = {
             enemy.kill();
         }
     },
-    
+
     update: function () {
-        // -- 
+        // --
+        this.uibutton.update()
         this.enemies.setAll('x', 1, true, true, 1);
         this.enemies.forEach(this.checkEnemy, this, true);
         this.fire.forEach(this.checkFire, this, true);
--- pavo project/src/Tower.js
+++ pavo project/src/Tower.js
@@ -1,16 +1,33 @@
 /* Class File for Tower*/
-Tower = function (game, x, y, targets) {
+Tower = function (game, x, y, key, bulletkey) {
   // Define null variables for tower class
-  Phaser.Sprite.call(this, game, x, y, 'tower')
-  this.weapon = game.add.weapon(30, 'bullet');
+  Phaser.Sprite.call(this, game, x, y, key);
+  this.anchor.x = 0.5;
+  this.anchor.y = 0.5;
+  this.weapon = game.add.weapon(30, bulletkey);
 
   // kills bullet if left world
-  this.weapon.bulletKillDistance = 200;
+  this.weapon.bulletKillDistance = 100;
   this.weapon.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
   this.weapon.bulletSpeed = 600;
   this.weapon.fireRate = 700;
   // tracks the pos of the tower
   this.weapon.trackSprite(this,0,0,true);
+  // circle for range
+//  var circleShell = this.game.add.sprite(0,0);
+  var graphics = this.game.add.graphics(0, 0);
+
+
+    // graphics.lineStyle(2, 0xffd900, 1);
+
+    graphics.beginFill(0, 0);
+    graphics.lineStyle(5, 500, 0)
+    graphics.drawCircle(0, 0, (this.weapon.bulletKillDistance *2));
+    //circleShell.addChild(graphics);
+    this.addChild(graphics);
+    // Enable physics on the sprite (as graphics objects cannot have bodies applied)
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+    //this.game.physics.enable(this.children[0], Phaser.Physics.ARCADE);
 };
 
 Tower.prototype = Object.create(Phaser.Sprite.prototype);
@@ -18,18 +35,70 @@ Tower.prototype.constructor = Tower;
 
 Tower.prototype.fire = function (target) {
   this.weapon.fireAtSprite(target);
-    game.physics.arcade.overlap(target, this.weapon.bullets, collisionHandler, null, this);
+  game.physics.arcade.overlap(target, this.weapon.bullets, collisionHandler, null, this);
+  //game.physics.arcade.overlap(target, this, collisionHandler, null, this);
+  //game.physics.arcade.collide(target, this.children[0], collisionHandler, null, this);
 };
 
 Tower.prototype.update = function () {
 
 };
 
-/* Static function for loading assets*/
-Tower.myload = function(game) {
+/****************Static functions************************/
+Tower.myload = function (game) {
   game.load.image('tower', 'img/tower.png');
   game.load.image('bullet', 'img/explosion.png');
 }
+
+/*creates a group that towers can be added to*/
+Tower.createGroup = function (game) {
+  var t = game.add.group();  // create a new group in the phaser game object
+  t.classType = Tower;  // set the classType to be our extended sprite class 'Tower'
+  return t;             // returns a refernce to this group
+}
+
+/*Creates a UI button *
+*key represents the button image key
+*place represets the image that will be glues to mouse
+*list represents the group created by Tower.creatGroup() */
+
+ function createTowerButton(game, x, y, key, place, list) {
+  this.game = game; // sets the current game
+  this.towerList = list; // sets the tower group
+  this.placeHolder = game.add.sprite(game.input.x, game.input.y, place);    // add a sprite that hovers around the mouse
+  this.placeHolder.anchor.set(0.5,0.5);                                    // sets sprite to have centered origin
+  this.placeHolder.visible = false;                                        // hide this sprite
+  this.towerButton = game.add.button(x, y, key, null , game, 2,1,0);      // add the button to the x, y
+
+  // Displays Towers "range" when adding
+  var graphics = this.game.add.graphics(0, 0);
+  graphics.beginFill(0xFF0000, 0);
+  graphics.lineStyle(2, 0xffd900, 1)
+  graphics.drawCircle(0, 0, (100 *2));
+  this.placeHolder.addChild(graphics);
+};
+
+createTowerButton.prototype = {
+  // Call Create in the levels create function after the obj as been made
+  create: function ()  {
+    this.towerButton.onInputDown.add(showTower, this);                  // add listener for clicking button down
+    this.towerButton.onInputUp.add(buildTower, this);                  // add listener for releasing after clicking hte button
+    function showTower () {
+      this.placeHolder.visible = true;                                // when mouse pressed down show the "fake tower"
+    }
+    function  buildTower() {
+      this.placeHolder.visible = false;                             // when button let go build the tower on the x, y
+      this.towerList.create(this.game.input.x, this.game.input.y, 'tower', 'bullet');
+    }
+  },
+  // call update in the levels update function
+  update: function () {
+    this.placeHolder.x = this.game.input.mousePointer.worldX;
+    this.placeHolder.y = this.game.input.mousePointer.worldY;
+  }
+};
+
+
 //  Called if the bullet hits one of the veg sprites
 collisionHandler = function (sprite, bullet) {
     bullet.kill();
