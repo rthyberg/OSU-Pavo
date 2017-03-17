063af0c99bc8ee5281cf2ec050d8c21c9d04ddd2
Ving Trung <vtrung>
2017-03-16 21:32:33 -0700

    remove demo links

--- pavo project/src/StartMenu.js
+++ pavo project/src/StartMenu.js
@@ -65,74 +65,74 @@ TowerDefense.StartMenu.prototype = {
         
         // **** DEMO MENU *** 
         // Set Menu Items
-        var text = game.add.text(game.world.centerX-300, game.world.centerY-100, "Demo1", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        text.anchor.set(0.5);
-        text.inputEnabled = true;
-        text.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("LevelOne");
-        }, this);
-        
-        var text2 = game.add.text(game.world.centerX-300, game.world.centerY-75, "Demo2", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        text2.anchor.set(0.5);
-        text2.inputEnabled = true;
-        text2.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("Game");
-        }, this);
-        
-        var text3 = game.add.text(game.world.centerX-300, game.world.centerY-50, "Demo3", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        text3.anchor.set(0.5);
-        text3.inputEnabled = true;
-        text3.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("LevelOne");
-        }, this);
-        
-        var text4 = game.add.text(game.world.centerX-300, game.world.centerY-25, "WaveDemo", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        text4.anchor.set(0.5);
-        text4.inputEnabled = true;
-        text4.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("WaveDemo");
-        },this);
-          
-        var text5 = game.add.text(game.world.centerX-300, game.world.centerY, "PathBuildDemo", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        text5.anchor.set(0.5);
-        text5.inputEnabled = true;
-        text5.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("PathBuildDemo");
-        },this);
-        
-        var textDarkRoom = game.add.text(game.world.centerX-300, game.world.centerY+25, "DarkRoom", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        textDarkRoom.anchor.set(0.5);
-        textDarkRoom.inputEnabled = true;
-        textDarkRoom.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("DarkRoom");
-        },this);
-        
-        var textSpookRoom = game.add.text(game.world.centerX-300, game.world.centerY+50, "SpookRoom", { font: "20px Arial", fill: "#ff0044", align: "left" });
-        textSpookRoom.anchor.set(0.5);
-        textSpookRoom.inputEnabled = true;
-        textSpookRoom.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            game.state.start("SpookRoom");
-        },this);
-        
-        var textAlpha = game.add.text(game.world.centerX-300, game.world.centerY+75, "LevelAlpha", { font: "20px Arial", fill: "#ff0044", align: "center" });
-        textAlpha.anchor.set(0.5);
-        textAlpha.inputEnabled = true;
-        textAlpha.events.onInputDown.add(function(){
-            this.soundmanager.stop();
-            this.soundmanager.jingle.play();
-            var pic = game.add.image(game.world.centerX, game.world.height, 'bgblack');
-            var pictxt = game.add.image(game.world.centerX, game.world.centerY, 'loader');
-            pic.anchor.setTo(0.5, 1);
-            pictxt.anchor.setTo(0.5, 1);
-            game.time.events.add(Phaser.Timer.SECOND * 5, fadePicture, this);
-        }, this);
+//        var text = game.add.text(game.world.centerX-300, game.world.centerY-100, "Demo1", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        text.anchor.set(0.5);
+//        text.inputEnabled = true;
+//        text.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("LevelOne");
+//        }, this);
+//        
+//        var text2 = game.add.text(game.world.centerX-300, game.world.centerY-75, "Demo2", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        text2.anchor.set(0.5);
+//        text2.inputEnabled = true;
+//        text2.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("Game");
+//        }, this);
+//        
+//        var text3 = game.add.text(game.world.centerX-300, game.world.centerY-50, "Demo3", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        text3.anchor.set(0.5);
+//        text3.inputEnabled = true;
+//        text3.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("LevelOne");
+//        }, this);
+//        
+//        var text4 = game.add.text(game.world.centerX-300, game.world.centerY-25, "WaveDemo", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        text4.anchor.set(0.5);
+//        text4.inputEnabled = true;
+//        text4.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("WaveDemo");
+//        },this);
+//          
+//        var text5 = game.add.text(game.world.centerX-300, game.world.centerY, "PathBuildDemo", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        text5.anchor.set(0.5);
+//        text5.inputEnabled = true;
+//        text5.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("PathBuildDemo");
+//        },this);
+//        
+//        var textDarkRoom = game.add.text(game.world.centerX-300, game.world.centerY+25, "DarkRoom", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        textDarkRoom.anchor.set(0.5);
+//        textDarkRoom.inputEnabled = true;
+//        textDarkRoom.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("DarkRoom");
+//        },this);
+//        
+//        var textSpookRoom = game.add.text(game.world.centerX-300, game.world.centerY+50, "SpookRoom", { font: "20px Arial", fill: "#ff0044", align: "left" });
+//        textSpookRoom.anchor.set(0.5);
+//        textSpookRoom.inputEnabled = true;
+//        textSpookRoom.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            game.state.start("SpookRoom");
+//        },this);
+//        
+//        var textAlpha = game.add.text(game.world.centerX-300, game.world.centerY+75, "LevelAlpha", { font: "20px Arial", fill: "#ff0044", align: "center" });
+//        textAlpha.anchor.set(0.5);
+//        textAlpha.inputEnabled = true;
+//        textAlpha.events.onInputDown.add(function(){
+//            this.soundmanager.stop();
+//            this.soundmanager.jingle.play();
+//            var pic = game.add.image(game.world.centerX, game.world.height, 'bgblack');
+//            var pictxt = game.add.image(game.world.centerX, game.world.centerY, 'loader');
+//            pic.anchor.setTo(0.5, 1);
+//            pictxt.anchor.setTo(0.5, 1);
+//            game.time.events.add(Phaser.Timer.SECOND * 5, fadePicture, this);
+//        }, this);
         
 
         function fadePicture(){
